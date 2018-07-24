be7198185fd2cbab4b1818f15fbd98b202e39444
Anmol Sharma <anmol.sharma293@gmail.com>
2018-07-24 14:37:35 -0700

    another change to make paths compatible

--- src/validate.py
+++ src/validate.py
@@ -57,7 +57,7 @@ options, remainder = parser.parse_args()
 # set the model name to load here
 options.defmodelfile = "isensee"
 options.grade = "Combined"
-options.model_name = "/home/anmol/mounts/cedar-rm/scratch/asa224/model-staging/BRATS_E160--0.78.h5"
+options.model_name = os.path.join(mount_path_prefix, "scratch/asa224/model-staging/BRATS_E2_WITH_PERMUTE_BLUR_AFFINE.h5")
 options.validate_on = "2018"
 # ---------------------------------------------------------------------
 
